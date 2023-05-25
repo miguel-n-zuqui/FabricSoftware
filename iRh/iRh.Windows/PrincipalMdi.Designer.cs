876e7e0c5d0c3eba8f265728aa3ac06e790d0b89
miguel-n-zuqui <miguelnatalezuqui@gmail.com>
2023-05-25 15:31:52 -0300

    ADICIONADO IMAGEM

--- iRh/iRh.Windows/PrincipalMdi.Designer.cs
+++ iRh/iRh.Windows/PrincipalMdi.Designer.cs
@@ -172,8 +172,11 @@ namespace iRh.Windows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::iRh.Windows.Properties.Resources.Fundo_tela_principal;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.menuPrincipal);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuPrincipal;
